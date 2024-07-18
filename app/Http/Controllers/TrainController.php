fc90851d6b3d1ffb1add6dceb762ab5f0e3a36f8
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-18 18:06:24 +0200

    train datas from db are now shown in main page

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
 
     public function index(){
-        return 'ciao sono index';
+        //prendo i dati dal database
+        $trains = Train::all();
+        return view('trains.index', compact('trains'));
     }
     //
 }
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -4,7 +4,22 @@
 @section('main-content')
 
 <h1>I treni di oggi:</h1>
+<section>
+    @foreach ($trains as $train)
+        <div>
+            <h2>{{$train->azienda}}</h2>
+            <h3>Stazione di partenza:</h3><p>{{$train->stazione_di_partenza}}</p>
+            <h3>Stazione di arrivo:</h3><p>{{$train->stazione_di_arrivo}}</p>
+            <h3>Orario di partenza:</h3><p>{{$train->orario_di_partenza}}</p>
+            <h3>Orario di arrivo:</h3><p>{{$train->orario_di_arrivo}}</p>
+            <h3>Codice:</h3><p>{{$train->codice_treno}}</p>
+            <h3>Numero Carrozze</h3><p>{{$train->numero_carrozze}}</p>
+            <p>{{ $train->in_orario ? 'in orario' : 'in ritardo' }}</p>        </div>
+
+    @endforeach
+</section>
+
 
 @endsection
 
-@endsection
+
