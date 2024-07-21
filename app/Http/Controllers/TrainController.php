4232cbff156a35cca1a26beed796df91fa4b4226
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-21 20:12:09 +0200

    set a seeder to create random data with laravel Faker

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,6 +9,8 @@ class TrainController extends Controller
 {
 
     public function index(){
+        $date = '2024-07-20';
+        $trains = Train::whereDate('orario_di_partenza', $date)->get();
         //prendo i dati dal database
         $trains = Train::all();
         return view('trains.index', compact('trains'));
--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
+use App\Models\train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
+
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->company(2, true);
+            $train->stazione_di_partenza = $faker->city();
+            $train->stazione_di_arrivo = $faker->city();
+            $train->orario_di_partenza = $faker->time();
+            $train->orario_di_arrivo = $faker->time();
+            $train->codice_treno = $faker ->randomNumber(5, true);
+            $train->numero_carrozze = $faker->randomNumber(3,true);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
+        }
     }
 }
